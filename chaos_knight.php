1d3595d0ad3fff2b068f457ff487d8557fd43bd0
neroly <artempshik@yandex.ru>
2024-11-28 19:04:19 +0300

    strenght

---
+++ chaos_knight.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/b8991598b2.js"></script>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="css/promo.css">
+    <link rel="stylesheet" href="css/last.css">
+    <link rel="stylesheet" href="css/hero.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:" rel="stylesheet">
+
+    <style>
+    body {
+        font-family: 'Montserrat';font-size: 22px;
+    }
+    </style>
+    <title></title>
+</head>
+<body style="background-image: url('fonts/mainfont.svg');
+background-repeat: no-repeat;
+background-size: cover;
+background-color: #212121;">
+        <header>
+            <div class="top_row">
+                <div class="container">
+                    <div class="col-50">
+                        <ul class="left_side">
+                            <div class="menu_item">
+                                <div class="dota_logo_menu">
+                                    <a href="index.php" class="dota_logo">
+                                        <img src="img/dotalogo.svg" alt="" class="dota_logo_img">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="menu_item">
+                                <details class="menu_menu_more">
+                                <summary>
+                                    <img src="img/more.svg" alt="" class="menu_menu_img">
+                                </summary>
+                                <div class="more_line">
+                                    <ul class="menu_more">
+                                        <div class="item">
+                                            <a href="#" class="your_stats">
+                                                <span class="vibor">личная статистика</span> 
+                                            </a>
+                                        </div>
+                                        <div class="item">
+                                            <a href="#" class="meta_herous">
+                                                <span class="vibor">meta herous</span>
+                                            </a>
+                                        </div>
+                                        <div class="item">
+                                            <a href="#" class="top_players">
+                                                <span class="vibor">лучшие игроки</span>
+                                            </a>
+                                        </div>
+                                    </ul>
+                                </div>
+                                </details>
+                            </div>
+                        </ul>
+                    </div>
+                    <div class="col-50">
+                        <ul class="right_side">
+                            <div class="menu_item">
+                                <a href="#" class="mail_href">
+                                    <img src="img/mail.svg" alt="" class="mail_img">
+                                </a>
+                            </div>
+                            <div class="menu_item">
+                                <a href="#" class="kolokol_href">
+                                    <img id="image" src="img/kolokol.svg" onClick="imgsrc();" class="kolokol_img">
+                                </a>
+                            </div>
+                            <div class="menu_item">
+                                <a href="#" class="setings_href">
+                                    <img src="img/Layer 10.svg" alt="" class="setings_img">
+                                </a>
+                            </div>
+                            <div class="menu_item">
+                            <a href="login.php" class="btn">Login</a>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main>
+        <div class="name">
+            <img src="img\heroavatar\chaos_knight.webp" alt="">
+            <h2>Chaos Knight</h2>
+        </div>
+        <div class="info">
+            <p>
+               Краткое описание героя<br><br> Chaos Knight — сам по себе армия. Он способен призывать кавалерию своих двойников и разрушать постройки врага, а когда покажутся защитники, он расколет реальность и перенесёт себя и свои копии на расстояние, достаточное для рокового взмаха булавы.
+            </p> 
+               <br>Способности:<br> <img src="imgskills\chaos_knight_chaos_bolt.png" alt=""><p1>Chaos Bolt</p1>
+                <br>Выпускает таинственный заряд энергии в указанного врага, оглушая его на случайное время и нанося случайный урон.
+                <br>
+                    <img src="imgskills\chaos_knight_reality_rift.png" alt=""><p1>Reality Rift</p1>
+               <br>Переносит героя, все его иллюзии и выбранного врага в точку между владельцем способности и жертвой. Уменьшает броню цели на 6 сек.
+               <br>
+                    <img src="imgskills\chaos_knight_chaos_strike.png" alt=""><p1>Chaos Strike</p1>
+               <br>Герой и его иллюзии имеют шанс совершить критическую атаку случайной силы и восстановить себе здоровье на часть от нанесённого урона. Урон крипам умножается на 2, но вампиризм от них уменьшается на 70%.
+.               <br>
+                    <img src="imgskills\chaos_knight_phantasm.png" alt=""><p1>Phantasm</p1>
+               <br>Призывает несколько копий героя из параллельных измерений. Копии являются иллюзиями, наносят 100% урона, а получают 350% урона.
+               <br>
+           
+        </div>
+        </main>

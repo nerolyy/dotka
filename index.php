e83e5d1897e38a2d95ae64a658930001fdfa0a4f
ПЛАТИНОВЫЙ МОРФЛИНГ АНТОНА ДУРАЧЬЁ <153272210+dmitry487@users.noreply.github.com>
2024-11-28 12:19:36 +0300

    footer all stiles

--- index.php
+++ index.php
@@ -173,17 +173,14 @@ z-index:10">
             </div>
         </div>
 </promo>
-<div class="container">
-  <footer class="py-3 my-4">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
-    </ul>
-    <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
-  </footer>
-</div>
+<footer class="py-3 my-4">
+    <div class="container">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+          <li class="nav-item"><a href="your_stats.php" class="nav-link px-2 text-body-secondary text-white">личная статистика</a></li>
+          <li class="nav-item"><a href="meta_herous.php" class="nav-link px-2 text-body-secondary text-white">meta herous</a></li>
+          <li class="nav-item"><a href="best_players.php" class="nav-link px-2 text-body-secondary text-white">лучшие игроки</a></li>
+        </ul>
+    </div>
+</footer>
 </body>
 </html>

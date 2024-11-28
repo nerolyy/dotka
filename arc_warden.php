7d8423e095071f4022f633a68308561de6898cb5
ПЛАТИНОВЫЙ МОРФЛИНГ АНТОНА ДУРАЧЬЁ <153272210+dmitry487@users.noreply.github.com>
2024-11-28 19:56:21 +0300

    arc warden

--- arc_warden.php
+++ arc_warden.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/b8991598b2.js"></script>
-    <link rel="stylesheet" href="css/reset.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="header.css">
-    <link rel="stylesheet" href="css/promo.css">
-    <link rel="stylesheet" href="css/last.css">
-    <link rel="stylesheet" href="css/hero.css">
+    <link rel="stylesheet" href="css/herous.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/css2?family=Kanit:" rel="stylesheet">
 
@@ -20,93 +19,78 @@
     </style>
     <title></title>
 </head>
-<body style="background-image: url('fonts/mainfont.svg');
+<header class="p-3 bg-dark text-white">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="index.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                    <img class="bi me-2 tmsk" src="img/dotalogo.svg" width="109" height="22" role="img" aria-label="Bootstrap">
+                </a>
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="your_stats.php" class="nav-link px-2 text-white tmsk">личная статистика</a></li>
+                    <li><a href="meta_herous.php" class="nav-link px-2 text-white tmsk">meta herous</a></li>
+                    <li><a href="best_players.php" class="nav-link px-2 text-white tmsk">лучшие игроки</a></li>
+                </ul>
+
+                <div class="text-end menu_item">
+                    <a href="#" class="lk">
+                        <p><?php isset($_COOKIE['login'])?></p>
+                    </a>
+                </div>
+                    <?php
+                                if(isset($_COOKIE['login']))
+                                    echo '<div class="text-end">
+                                        <a href="user_main.php" type="button" class="btn btn-outline-light me-2 lk">Личный кабинет</a>
+                                    </div>';
+                                else
+                                    echo '<div class="text-end">
+                                    <a type="button" href="login.php" class="btn btn-outline-light me-2">Login</a>
+                                    <a type="button" href="auth.php" class="btn btn-warning">Sign-up</a>
+                                    </div>'
+                    ?>
+            </div>
+        </div>
+</header>
+<main style="background-image: url('fonts/greyfade.jpg');
 background-repeat: no-repeat;
 background-size: cover;
 background-color: #212121;">
-        <header>
-            <div class="top_row">
-                <div class="container">
-                    <div class="col-50">
-                        <ul class="left_side">
-                            <div class="menu_item">
-                                <div class="dota_logo_menu">
-                                    <a href="index.php" class="dota_logo">
-                                        <img src="img/dotalogo.svg" alt="" class="dota_logo_img">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="menu_item">
-                                <details class="menu_menu_more">
-                                <summary>
-                                    <img src="img/more.svg" alt="" class="menu_menu_img">
-                                </summary>
-                                <div class="more_line">
-                                    <ul class="menu_more">
-                                        <div class="item">
-                                            <a href="#" class="your_stats">
-                                                <span class="vibor">личная статистика</span> 
-                                            </a>
-                                        </div>
-                                        <div class="item">
-                                            <a href="#" class="meta_herous">
-                                                <span class="vibor">meta herous</span>
-                                            </a>
-                                        </div>
-                                        <div class="item">
-                                            <a href="#" class="top_players">
-                                                <span class="vibor">лучшие игроки</span>
-                                            </a>
-                                        </div>
-                                    </ul>
-                                </div>
-                                </details>
-                            </div>
-                        </ul>
-                    </div>
-                    <div class="col-50">
-                        <ul class="right_side">
-                            <div class="menu_item">
-                                <a href="#" class="mail_href">
-                                    <img src="img/mail.svg" alt="" class="mail_img">
-                                </a>
-                            </div>
-                            <div class="menu_item">
-                                <a href="#" class="kolokol_href">
-                                    <img id="image" src="img/kolokol.svg" onClick="imgsrc();" class="kolokol_img">
-                                </a>
-                            </div>
-                            <div class="menu_item">
-                                <a href="#" class="setings_href">
-                                    <img src="img/Layer 10.svg" alt="" class="setings_img">
-                                </a>
-                            </div>
-                            <div class="menu_item">
-                            <a href="login.php" class="btn">Login</a>
-                            </div>
-                        </ul>
-                    </div>
+    <div class="all-hero-inf">
+        <div class="hero-continer">
+            <div class="hero-vido_container">
+                <div class="antimage video">
+                    <video class="_22nJ5nsfHDS2jEscPEne0-" poster="https://cdn.akamai.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/arc_warden.mov" autoplay preload="auto" loop playsinline>
+                        <source type="video/mp4; codecs=&quot;hvc1&quot;" src="https://cdn.akamai.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/arc_warden.webm?undefined">
+                        <img src="https://cdn.akamai.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/arc_warden.png">
+                    </video>
                 </div>
             </div>
-        </header>
-        <main>
-        <div class="name">
-            <img src="img\heroavatar\arcwarden.webp" alt="">
-            <h2>Arc Warden</h2>
-        </div>
-        <div class="info">
-            <p>
-               Краткое описание героя<br><br> Зет, Arc Warden — разбитый осколок той древней мощи, что породила самих Древних, и он намерен прекратить схватку Света и Тьмы. Сковывайте одиноких врагов потоками энергии или искажайте пространство, создавая защитное поле для союзников. Призывайте призрачные искры, нападающие на приблизившихся противников, или же полную копию самого себя со всеми предметами и способностями, способную сокрушить неприятеля.
-            </p> 
-                <br>Способности:<br> <img src="imgskills\arc_warden_flux.png" alt=""><p1>Flux</p1>
-            <br>Охватывает одинокого врага нестабильной энергией, замедляя его передвижение и нанося урон. Урон блокируется, если рядом с целью есть её союзники.<br>
-                <img src="imgskills\arc_warden_magnetic_field.png" alt=""><p1>Magnetic Field</p1>
-            <br> Герой создаёт вокруг себя круглое магнитное поле.<br>
-                <img src="imgskills\arc_warden_spark_wraith.png" alt=""><p1>Spark Wraith</p1>
-            <br>Призывает призрачную искру, которая медленно материализуется и сторожит выделенную область. При появлении врага искра бросается на него и взрывается, нанося жертве магический урон и замедляя её. Наносит существам, не являющимся героями, на 40% больше урона.<br>
-                <img src="imgskills\arc_warden_tempest_double.png" alt=""><p1>Tempest Double</p1>
-            <br>Сконцентрировав свои осколки в единую сущность, герой создаёт свою идеальную электрическую копию, способности которой имеют эффекты противоположной команды. Копия может использовать все предметы оригинала, кроме многих расходуемых и тех, что теряются при смерти, и имеет отдельное время перезарядки для них.
-            <br>Копия теряет себя, если находится на слишком большом расстоянии от своего создателя, отчего ей наносят больше урона.
-           
-        </div>
-        </main>
+            <div class="text-hero-container">
+                <div class="atribut-row">
+                    <img src="img/hero_agility.png" class="atribut-png">
+                    <div class="atribut_name">Ловкость</div>
+                </div>
+                <div class="h1-tekst">ARC WARDEN</div>
+                <div class="deskription-hero-text">Копирует себя, чтобы вести войну на два фронта</div>
+                <div class="b-deskription-hero-text-container">
+                    <div class="hero-story">
+                        <div class="h-s-text f-b">  <span class="f-b">забирать у врагов ману</span> каждым ударом или 
+                        <span class="f-b">телепортироваться на небольшие расстояния</span>, что не позволяет врагам загнать его в угол.
+                    </div>
+                    <div>
+                        <div class="atack-type">Тип атаки</div>
+                        <div class="atack-type-row">
+                            <img class="atack-type-img" src="https://cdn.akamai.steamstatic.com/apps/dota2/images/dota_react/icons/ranged.svg">
+                        <div class="atack-range">Дальний бой</div>
+                    </div>
+                </div>
+                <div>
+                    <div class="complexity">Сложность</div>
+                    <div class="complexity-row">
+                        <div class="romb w-r"></div>
+                        <div class="romb w-r"></div>
+                        <div class="romb w-r"></div>
+                    </div>
+                </div> 
+    </div>
+</main>

0db056dda4951089ac8bcfd3dbe8a20093de6623
neroly <artempshik@yandex.ru>
2024-11-28 14:03:29 +0300

    ff

--- index.php
+++ index.php
@@ -173,6 +173,32 @@ z-index:10">
                     <h1 class="hero_name">Lina</h1>
                 </a>
             </div>
+            <div class="category_box_row">
+                    <a href="antimage.php" class="category_box bg-dark">
+                        <img src="fonts/Anti-mage.svg" stile="width=115px; height=115px;">
+                        <h1 class="hero_name">Anti-mage</h1>
+                    </a>
+                    <a href="arc_warden.php" class="category_box bg-dark">
+                        <img src="fonts/Arc.svg" stile="width=115px; height=115px;">
+                        <h1 class="hero_name">Arc Warden</h1>
+                    </a>
+                    <a href="bloodseeker.php" class="category_box bg-dark">
+                        <img src="fonts/bloodsecer.svg" stile="width=115px; height=115px;">
+                        <h1 class="hero_name">Bloodseker</h1>
+                    </a>
+                    <a href="bounty_hunter.php" class="category_box bg-dark">
+                        <img src="fonts/Bounty_hunter.svg" stile="width=115px; height=115px;">
+                        <h1 class="hero_name">Bounty Hunter</h1>
+                    </a>
+                    <a href="clinkz.php" class="category_box bg-dark">
+                        <img src="fonts/clinz.svg" stile="width=115px; height=115px;">
+                        <h1 class="hero_name">Clinkz</h1>
+                    </a>
+                    <a href="drow_ranger.php" class="category_box bg-dark">
+                        <img src="fonts/drow ranger.svg" stile="width=115px; height=115px;">
+                        <h1 class="hero_name">Drow Ranger</h1>
+                    </a>
+            </div>
         </div>
     </promo>
 </body>

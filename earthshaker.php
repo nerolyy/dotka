756194793d0e88b69dcdf2567aabe602a3fb8eb1
ПЛАТИНОВЫЙ МОРФЛИНГ АНТОНА ДУРАЧЬЁ <153272210+dmitry487@users.noreply.github.com>
2024-12-03 17:21:33 +0300

    arc and shaker

--- earthshaker.php
+++ earthshaker.php
@@ -59,9 +59,9 @@ background-color: #212121;">
         <div class="hero-continer">
             <div class="hero-vido_container">
                 <div class="antimage video">
-                    <video class="_22nJ5nsfHDS2jEscPEne0-" poster="fonts/antimage.mov" autoplay preload="auto" loop playsinline>
-                        <source type="video/mp4; codecs=&quot;hvc1&quot;" src="https://cdn.akamai.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/antimage.mov">
-                        <source type="video/webm" src="https://cdn.akamai.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/antimage.webm?undefined"><img src="https://cdn.akamai.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/antimage.png">
+                    <video class="_22nJ5nsfHDS2jEscPEne0- " poster="https://cdn.akamai.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/earthshaker.png" autoplay preload="auto" loop playsinline>
+                        <source type="video/mp4; codecs=&quot;hvc1&quot;" src="https://cdn.akamai.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/earthshaker.mov">
+                        <source type="video/webm" src="https://cdn.akamai.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/earthshaker.webm?undefined"><img src="https://cdn.akamai.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/earthshaker.png">
                     </video>
                 </div>
             </div>
